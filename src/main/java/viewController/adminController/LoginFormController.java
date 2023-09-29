7affbd79630190da542280790bed915414f17a80
unknown <hirokan9298@gmail.com>
2023-09-29 15:48:10 +0530

    Admin and User login controlled

--- src/main/java/viewController/adminController/LoginFormController.java
+++ src/main/java/viewController/adminController/LoginFormController.java
@@ -1,5 +1,7 @@
 package viewController.adminController;
 
+import dto.UserProfileDto;
+import jakarta.xml.bind.DatatypeConverter;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -9,8 +11,15 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import service.ServiceFactory;
+import service.UserService;
 
 import java.io.IOException;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class LoginFormController {
 
@@ -28,28 +37,69 @@ public class LoginFormController {
 
     }
 
-    public void btnLoginAction(ActionEvent actionEvent) throws IOException {
-////temp for testing
+    public void btnLoginAction(ActionEvent actionEvent) throws IOException, SQLException, NoSuchAlgorithmException, ClassNotFoundException {
+      boolean Istrue=checkCredentials(txtUsername.getText() ,txtPassword.getText());
 
+      if(Istrue){
 
-       if (txtUsername.getText().equals("admin")&& txtPassword.getText().equals("123")) {
-           Parent pane1 = FXMLLoader.load(getClass().getResource("/view/adminView/dashboard-form.fxml"));
-           Scene scn =new Scene(pane1);
-           Stage stg = (Stage) this.rootLogin.getScene().getWindow();
-           stg.setScene(scn);
-           stg.setTitle("admin Dashboard");
-           stg.show();
-
-        }else{
-           ////temp for testing
            Parent pane1 = FXMLLoader.load(getClass().getResource("/view/userView/user-dashboard-form.fxml"));
            Scene scn =new Scene(pane1);
            Stage stg = (Stage) this.rootLogin.getScene().getWindow();
            stg.setScene(scn);
-           stg.setTitle("admin Dashboard");
+           stg.setTitle("User Dashboard");
            stg.show();
+
+        } else{new Alert(Alert.AlertType.ERROR,"invalid credentials").show(); }
+    }
+
+    public boolean checkCredentials(String u ,String p) throws SQLException, ClassNotFoundException, NoSuchAlgorithmException {
+
+        MessageDigest md = MessageDigest.getInstance("MD5");
+        md.update(p.getBytes());
+        byte[] digest = md.digest();
+        String myHash = DatatypeConverter.printHexBinary(digest).toLowerCase();
+
+        UserService us=(UserService) ServiceFactory.getInstance().getService(ServiceFactory.ServiceType.USER);
+        ArrayList<UserProfileDto> udtos =us.getAll();
+
+        for ( UserProfileDto d : udtos  ) {
+
+            if (  d.getUserName().equals(u ) && d.getPassword().equals(myHash) ){
+
+                return true;
+
+            }
+
+        }
+        return false;
+    }
+
+    public boolean checkCredentialsAdmin(String u ,String p) throws SQLException, ClassNotFoundException, NoSuchAlgorithmException {
+
+        MessageDigest md = MessageDigest.getInstance("MD5");
+        md.update(p.getBytes());
+        byte[] digest = md.digest();
+        String myHash = DatatypeConverter.printHexBinary(digest).toLowerCase();
+
+            if(  u.equals("admin")  && myHash.equals("202cb962ac59075b964b07152d234b70")  ){
+                return true;
+            }else{
+                new Alert(Alert.AlertType.ERROR,"invalid admin credentials").show();
+                return false ;
+
+            }
 
         }
 
+    public void btnAdminLogin(ActionEvent actionEvent) throws SQLException, NoSuchAlgorithmException, ClassNotFoundException, IOException {
+        if (checkCredentialsAdmin(txtUsername.getText() ,txtPassword.getText())) {
+            Parent pane1 = FXMLLoader.load(getClass().getResource("/view/adminView/dashboard-form.fxml"));
+            Scene scn =new Scene(pane1);
+            Stage stg = (Stage) this.rootLogin.getScene().getWindow();
+            stg.setScene(scn);
+            stg.setTitle("admin Dashboard");
+            stg.show();
+
+        }
     }
 }
